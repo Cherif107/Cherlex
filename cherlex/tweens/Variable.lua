043d1ebb9a99e2dcdc28f7b57f9ccfeab701111f
Cherry <78418245+Cherif107@users.noreply.github.com>
2023-03-04 17:48:47 +0100

    feef

--- cherlex/tweens/Variable.lua
+++ cherlex/tweens/Variable.lua
@@ -1,11 +1,28 @@
 local Tween = require 'cherlex.tweens.root.Tween'
+local util = require 'cherlex.util.StringUtil'
+
+local function get(object, fieldString)
+    local fields = util.split(fieldString, '.')
+    local dis = object
+    if #fields > 0 then
+        for i = 1, #fields do
+            dis = dis[fields[i]]
+        end
+    end
+    return dis
+end
+local function set(object, fieldString, value)
+    local fields = util.split(fieldString, '.')
+    local restOfFields = fieldString:sub(1, #fieldString - #fields[#fields])
+    get(object, restOfFields)[fields[#fields]] = value
+end
 
 local function normalizeValues(Object, Values)
     local V = {}
     for f, v in next, Values, nil do
        V[f] = {
-        startValue = Object[f],
-        range = v-Object[f]
+        startValue = get(Object, f),
+        range = v-get(Object, f)
        }
     end
     return V
@@ -15,7 +32,7 @@ end
 local TweenVariable = function(Object, Values, Duration, Options)
     local this = Tween(Duration, Options, function(twn, scale, elapsed)
         for field, value in next, twn.values, nil do
-            twn.object[field] = value.startValue + value.range * scale
+            set(twn.object, field, value.startValue + value.range * scale)
         end
     end)
     this.object = Object
