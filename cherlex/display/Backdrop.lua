306f61f784e3258308b761670d0685840eedd4ad
Cherry <78418245+Cherif107@users.noreply.github.com>
2023-03-10 14:34:45 +0100

    Delete cherlex directory

--- cherlex/display/Backdrop.lua
+++
@@ -1,8 +0,0 @@
--- local import = require 'import'
--- local Sprite = import 'Sprite'
--- local Point = import 'math.Point'
-
--- ---@class Backdrop a backdrop class 
--- local backdrop = function(tag)
---     local f = Sprite(tag)
--- end
